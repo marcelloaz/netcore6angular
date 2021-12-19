24be27928de163125e25e528bab0d273e5d7568a
Marcello Azevedo <imcello@hotmail.com>
2021-12-19 07:52:44 -0300

    inicial estruture projetct oauth angular netcore 6

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
+using API_CORE.Models;
+
+namespace API_CORE.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+        : base(options, operationalStoreOptions)
+    {
+        
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace API_CORE.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    
+}
